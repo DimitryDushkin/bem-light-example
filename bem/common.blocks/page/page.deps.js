829db1f65b53e49dd4f6571e0973241f76c8d613
Dmitry Dushkin <dndushkin@yandex-team.ru>
2015-08-18 22:26:18 +0300

    Более правильные зависимости для бандла

--- bem/common.blocks/page/page.deps.js
+++ bem/common.blocks/page/page.deps.js
@@ -1,14 +1,5 @@
 ({
     shouldDeps: [
-        { block: 'i-reset' },
-        { block: 'page-layout' },
-        { block: 'main-menu' },
-        { block: 'advert-search-form' },
-        { block: 'properties-list' },
-        { block: 'properties-search' },
-        { block: 'property' },
-        { block: 'property-type' },
-        { block: 'property-sort' },
         { elem: 'typography' }
     ]
 });
--- bem/desktop.bundles/index/index.bemdecl.js
+++ bem/desktop.bundles/index/index.bemdecl.js
@@ -1,3 +1,12 @@
 exports.blocks = [
-    { name: 'page' }
+    { name: 'page' },
+    { name: 'i-reset' },
+    { name: 'page-layout' },
+    { name: 'main-menu' },
+    { name: 'advert-search-form' },
+    { name: 'properties-list' },
+    { name: 'properties-search' },
+    { name: 'property' },
+    { name: 'property-type' },
+    { name: 'property-sort' }
 ];
