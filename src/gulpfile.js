a2c61a0939fb4d2621e51d91ed63d80005397e02
indra-uolles <smirnovanatalia2008@gmail.com>
2015-11-22 23:23:24 +0300

    add gulp for bro-sync

---
+++ src/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    terminal = require('child_process'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "../dist"
+        }
+    });
+});
+
+gulp.task('enb', function() {
+    terminal.exec("enb make", {}, function(error,stdout,stderr){
+         browserSync.reload();
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('blocks/**/*.styl', ['enb']);
+    gulp.watch('blocks/**/*.js', ['enb']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
